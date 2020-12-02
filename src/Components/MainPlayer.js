e143d50f4a75a491b56a97fdbc7255611fa5f413
= <garvitgalgat@gmail.com>
2020-12-02 14:44:28 +0530

    basic app components added

---
+++ src/Components/MainPlayer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MainPlayer() {
+    return (
+        <div>
+            MAINPLAYER
+        </div>
+    )
+}
--- src/Views/Player.js
+++ src/Views/Player.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Drawer from '../Components/Drawer'
+import MainPlayer from '../Components/MainPlayer'
+
+import Button from '@material-ui/core/Button'
 
 export default function Player() {
     return (
         <div>
-            MUSIC PLAYER
+            <Drawer />
+            <MainPlayer />
+            <Button variant="contained" color="primary">Hello World</Button>
         </div>
     )
 }
