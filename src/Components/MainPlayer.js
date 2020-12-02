5cce771b081132839a63bc4f6a09f4f2111fa14d
= <garvitgalgat@gmail.com>
2020-12-02 19:05:09 +0530

    resolved

--- src/Components/MainPlayer.js
+++ src/Components/MainPlayer.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import FolderIcon from '@material-ui/icons/Folder';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+const useStyles = makeStyles({
+    root: {
+        width: '100%',
+        position: 'fixed',
+        bottom: 0,
+        background: 'rgb(40,40,40)',
+        // color: 'rgb(0,0,0)',
+        padding: '2rem'
+    },
+    play: {
+        '& svg': {
+            fontSize: 50
+        }
+    },
 
-export default function MainPlayer() {
+});
+
+export default function LabelBottomNavigation() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState('play');
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
-        <div>
-            MAINPLAYER
-        </div>
-    )
+        <BottomNavigation value={value} onChange={handleChange} className={classes.root} >
+            <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
+            <BottomNavigationAction label="Previous" value="previous" icon={<ChevronLeftIcon />} />
+            <BottomNavigationAction label="Play" value="play" icon={<PlayCircleOutlineIcon />} className={classes.play} />
+            <BottomNavigationAction label="Next" value="next" icon={<ChevronRightIcon />} />
+            <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+        </BottomNavigation>
+    );
 }
--- src/Views/Player.js
+++ src/Views/Player.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import Drawer from '../Components/Drawer'
+// import Drawer from '../Components/Drawer'
 import MainPlayer from '../Components/MainPlayer'
 
-import Button from '@material-ui/core/Button'
+// import Button from '@material-ui/core/Button'
 
 export default function Player() {
     return (
         <div>
-            <Drawer />
             <MainPlayer />
             {/* <Button variant="contained" color="primary">Hello World</Button> */}
         </div>
